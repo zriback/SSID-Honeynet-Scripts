108e5ac4eaa2e4fa9eca8797dfd56552f127ea45
root <root@t-pot>
2024-04-08 04:50:46 +0000

    Update script

--- start_captures.py
+++ start_captures.py
@@ -3,32 +3,31 @@
 import socket
 import subprocess
 import threading
+import queue
 
-LISTEN_INT = '192.168.1.20'
+LISTEN_INT = '127.0.0.1'
 LISTEN_PORT = 54444
+SAVE_LOCATION = '/data/cowrie/captures'
 
 # file rotation is broken
 # TCPDUMP_CMD = 'tcpdump -i any -w /etc/caps/{conn_id}.pcap -C 20 -W 54'
-TCPDUMP_CMD = 'tcpdump -i any -w /etc/data/{conn_id}.pcap'
+TCPDUMP_CMD = 'tcpdump -i any -w {save_loc}/{conn_id}.pcap'
 
 # starts a tcpdump that will automatically terminate when receiving FIN packet from the given IP
 # TODO: make that true
 def start_tcpdump(conn_id, conn_ip):
-    cmd = TCPDUMP_CMD.format(conn_id=conn_id)
+    cmd = TCPDUMP_CMD.format(save_loc=SAVE_LOCATION, conn_id=conn_id)
 
-    print('Starting tcpdump...')
-    print(cmd)
+    # print('Starting tcpdump...')
+    # print(cmd)
     
-    tcpdump_process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    tcpdump_proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    return tcpdump_proc
 
-    try:
-        for line in iter(tcpdump_process.stdout.readline, ''):
-            print(line.strip())
-    finally:
-        tcpdump_process.terminate()
-        tcpdump_process.wait()
 
-def listen_for_connections(interface, port):
+def manage_connections(interface, port):
+    captures = {}
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((interface, port))
         server_socket.listen()
@@ -36,17 +35,26 @@ def listen_for_connections(interface, port):
         while True:
             client_socket, client_addr = server_socket.accept()
             with client_socket:
-                data = str(client_socket.recv(1024).decode())
-                conn_id = data.split()[0]
-                conn_ip = data.split()[1]
+                data = str(client_socket.recv(1024).decode()).split()
+                action = data[0]
+                conn_id = data[1]
+                conn_ip = data[2]
                 client_socket.close()
 
-            # start tcp dump process now
-            start_tcpdump(conn_id, conn_ip)
-
-                
+            # start or stop tcp dump processes
+            if action == 'open':
+                tcpdump_proc = start_tcpdump(conn_id, conn_ip)
+                captures[conn_id] = tcpdump_proc
+            elif action == 'close':
+                tcpdump_proc = captures.get(conn_id)
+                tcpdump_proc.terminate()
+                del captures[conn_id]
+            else:
+                # something went really wrong
+                exit(10)
+            
 
 if __name__ == '__main__':
-    listen_for_connections(LISTEN_INT, LISTEN_PORT)
+    manage_connections(LISTEN_INT, LISTEN_PORT)
 
 
