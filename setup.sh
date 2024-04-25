4050eab48ca15bb7a3da6d300126a8fd4f61e08f
root <root@t-pot>
2024-04-25 20:43:37 +0000

    Add debug line

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 # Sets up the backend from the scripts in this repo
 
+set -x
+
 cp client_transport.py /data/cowrie/client_transport.py
 cp cowrie-capture.service /etc/systemd/system/cowrie-capture.service
 cp log_mon.py /opt/tpot/etc/log_mon.py
@@ -10,3 +12,5 @@ cp start_captures.py /opt/tpot/etc/start_captures.py
 cp start_captures_service.sh /opt/tpot/etc/start_captures_service.sh
 cp tpot.yml-Offline /opt/tpot/etc/tpot.yml-Offline
 cp tpot.yml-Online /opt/tpot/etc/tpot.yml-Online
+
+set +x
