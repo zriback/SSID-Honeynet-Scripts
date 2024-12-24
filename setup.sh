fd48ce6882c96118ef069371ffb7f1de29c41083
zriback <zacharythecdr@gmail.com>
2024-12-23 22:40:04 -0500

    Delete auto-captures scripts and service files that are no longer necessary. Update so proper versions of scripts are only present where they are needed in the ansible directory. Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,9 @@
 set -x
 
 cp client_transport.py /data/cowrie/client_transport.py
-cp cowrie-capture.service /etc/systemd/system/cowrie-capture.service
-cp log_mon.py /opt/tpot/etc/log_mon.py
 cp server_transport.py /data/cowrie/server_transport.py
-cp start_captures_backend.py /opt/tpot/etc/start_captures.py
-cp start_captures_service.sh /opt/tpot/etc/start_captures_service.sh
 cp tpot.yml-Offline /opt/tpot/etc/tpot.yml-Offline
 cp tpot.yml-Online /opt/tpot/etc/tpot.yml-Online
+cp tpot.yml-Online /opt/tpot/etc/tpot.yml
 
 set +x
